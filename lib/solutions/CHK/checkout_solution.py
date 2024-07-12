fc8a5c446a0e44eefb86b3920c317d733c2cb37a
sbx_user1051 <sbx_user1051@169.254.22.213>
2024-07-12 09:27:04 +0000

    Fri Jul 12 09:27:04 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,19 @@
+def create_special_pricing(single_price, multi_price, multi_quantity):
+    def price(amount):
+        return (amount / multi_quantity) * multi_price + (
+            amount % multi_quantity
+        ) * single_price
+    return price
+
+def create_normal_pricing(single_price):
+    return lambda x: x*single_price
+
+prices = {"A": create_special_pricing(50, 130, 3), "B" : create_special_pricing(30, 45, 2),
+          "C"}
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     raise NotImplementedError()
+
