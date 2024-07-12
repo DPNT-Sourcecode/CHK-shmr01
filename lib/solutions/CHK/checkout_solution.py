91d43cb9cc80f4035563185fdead047475214ebe
sbx_user1051 <sbx_user1051@169.254.22.213>
2024-07-12 10:20:33 +0000

    Fri Jul 12 10:20:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,10 +2,9 @@ def create_special_pricing(multi_prices, multi_quantities):
     def price(amount, _basket):
         result = 0
         for multi_price, quantity in zip(multi_prices, multi_quantities):
-            print("calculating price", multi_price, quantity, amount)
             result += (amount // quantity) * multi_price
             if amount >= quantity:
-                amount -= amount % quantity
+                amount -= quantity
         return result
 
     return price
@@ -58,6 +57,7 @@ def checkout(skus):
         result += price_func(amount, basket)
         print("checking:", sku, basket, result)
     return result
+
 
 
 
--- test/solution_tests/SUM/test_chk.py
+++ test/solution_tests/SUM/test_chk.py
@@ -3,14 +3,14 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout:
     def test_checkout(self):
-        # assert checkout_solution.checkout("A") == 50
-        # assert checkout_solution.checkout("AA") == 100
+        assert checkout_solution.checkout("A") == 50
+        assert checkout_solution.checkout("AA") == 100
         assert checkout_solution.checkout("AAA") == 130
-        # assert checkout_solution.checkout("AAAA") == 180
-        # assert checkout_solution.checkout("B") == 30
-        # assert checkout_solution.checkout("X") == -1
-        # assert checkout_solution.checkout("BX") == -1
-        # assert checkout_solution.checkout("BA") == 80
+        assert checkout_solution.checkout("AAAA") == 180
+        assert checkout_solution.checkout("B") == 30
+        assert checkout_solution.checkout("X") == -1
+        assert checkout_solution.checkout("BX") == -1
+        assert checkout_solution.checkout("BA") == 80
 
     def test_checkout_r2(self):
         assert checkout_solution.checkout("E") == 40
@@ -21,6 +21,7 @@ class TestCheckout:
         # assert checkout_solution.checkout("EEEBB") == 150
         # assert checkout_solution.checkout("AEB") == 120
         # assert checkout_solution.checkout("AAAAA") == 200
+
 
 
 
